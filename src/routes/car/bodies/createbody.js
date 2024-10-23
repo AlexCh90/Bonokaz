c6e1669abb7cd7bbb55e3aab829d3e114a1e7bf3
Alex Chevroton <alex.chevroton@protonmail.com>
2024-10-23 16:58:28 +0200

    23/10/2024 - 16 h 58

--- src/routes/car/bodies/createbody.js
+++ src/routes/car/bodies/createbody.js
@@ -3,21 +3,21 @@ const { ValidationError, UniqueConstraintError } = require('sequelize')
 //const auth = require('../../../auth/auth')
 
 module.exports = (app) => {
-  app.post('/bodies', (req, res) => {
-    Body.create(req.body)
-      .then(carbody => {
-        const message = `La carrosserie ${req.body.name} a bien été créée.`
-        res.json({ message, data: carbody })
-      })
-      .catch(error => {
-        if(error instanceof ValidationError) {
-          return res.status(400).json({ message: error.message, data: error });
-        }
-        if(error instanceof UniqueConstraintError) {
-          return res.status(400).json({ message: 'error.message', data: error });
-        }
-        const message = `La carrosserie n'a pas pu être ajoutée. Réessayez dans quelques instants.`
-        res.status(500).json({ message, data: error })
-      })
-  })
+    app.post('/bodies', (req, res) => {
+      Body.create(req.body)
+        .then(carbody => {
+          const message = `La carrosserie ${req.body.name} a bien été créée.`
+          res.json({ message, data: carbody })
+        })
+        .catch(error => {
+          if(error instanceof ValidationError) {
+            return res.status(400).json({ message: error.message, data: error });
+          }
+          if(error instanceof UniqueConstraintError) {
+            return res.status(400).json({ message: 'error.message', data: error });
+          }
+          const message = `La carrosserie n'a pas pu être ajoutée. Réessayez dans quelques instants.`
+          res.status(500).json({ message, data: error })
+        })
+    })
 }
